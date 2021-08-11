3c6ec4ab196df3c334fafe178a8118cffff4733b
thanhnguyen2112 <72983910+thanhnguyen2112@users.noreply.github.com>
2021-08-11 21:24:36 +0700

    Update index.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -159,8 +159,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <img src="adminlte/dist/img/bkacad.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">BKACAD</span>
     </a>
 
     <!-- Sidebar -->
